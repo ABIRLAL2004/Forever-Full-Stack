fd5c3b9823d47bd6512740fffc5fcc4a8a30b183
ABIRLAL2004 <abirlalmishra13@gmail.com>
2025-09-23 14:00:27 +0530

    sloved vercel erroe

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,60 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { backendUrl } from '../App'
-import { toast } from 'react-toastify'
-import { assets } from '../assets/assets'
+import React from 'react'
 
-const Orders = ({ token }) => {
-  const [orders, setOrders] = useState([])
-
-  const fetchAllorders = async (params) => {
-
-    if (!token) {
-      return null;
-    }
-    try {
-      const responce = await axios.post(backendUrl + '/api/orders/list', {}, { headers: { token } })
-      if (responce.data.success) {
-        setOrders(responce.data.Orders)
-      } else {
-        toast.error(responce.data.message)
-      }
-
-    } catch (error) {
-      toast.error(error.message)
-    }
-
-  }
-  useEffect(() => {
-    fetchAllorders();
-  }, [token])
-
-
+const Orders = () => {
   return (
     <div>
-      <h3>Order Page</h3>
-      <div>
-        {
-          orders.map((Order, index) => {
-            <div key={index}>
-              <img src={assets.parcel_icon} alt="" />
-              <div>
-                {orders.items.map((item, index) => {
-                  if (index === orders.items.length-1) {
-                    return <p key={index}>{item.name}x {item.qusntity} <span> {item.size}</span></p>
-                  }
-                  else {
-
-                  }
-                })}
-              </div>
-
-            </div>
-
-          })
-        }
-      </div>
-
+      
     </div>
   )
 }
