cc75e715693bed27fc6d7b20c40ffc1539f70928
ABIRLAL2004 <abirlalmishra13@gmail.com>
2025-10-20 09:14:39 +0530

    Fix: corrected case-sensitive path for userModel

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -1,7 +1,7 @@
 import validator from "validator";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import userModel from "../models/userModel.js";
+import userModel from "../models/usermodel.js";
 
 const createToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "id" }); // optional expiry
